da1c111852e8f16668bde8ca6ba345dfd0962340
Rohan <rohangambhir.com@gmail.com>
2020-01-20 11:54:24 +0530

    First commit for CI job.

---
+++ src/main/java/com/nagarro/helloworld/ci/App.java
@@ -0,0 +1,13 @@
+package com.nagarro.helloworld.ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
