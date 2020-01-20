7fdff542b103a874b342f17664bac935fc73c2fa
rohangambhir92 <42044873+rohangambhir92@users.noreply.github.com>
2020-01-20 15:43:45 +0530

    check build failure

--- src/main/java/com/nagarro/helloworld/ci/App.java
+++ src/main/java/com/nagarro/helloworld/ci/App.java
@@ -6,7 +6,7 @@ package com.nagarro.helloworld.ci;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public stati void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
